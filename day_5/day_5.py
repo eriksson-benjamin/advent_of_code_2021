e8e4783739f15abb84a36cf342ce85ec6fdf3480
Benjamin Eriksson <j.benjamin.eriksson@gmail.com>
2021-12-28 16:22:55 +0100

    Finished day 5

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,23 +1,15 @@
 import numpy as np
 
 '''
-Part 1
+Part 1 and 2
 '''
 
 # Load coordinates
 C = np.loadtxt('input.txt', delimiter=',')
 
 # x are columns, y are rows
-X = np.array([C[:,0], C[:,2]], dtype='int')
-Y = np.array([C[:,1], C[:,3]], dtype='int')
-
-# Select coordinates where either x1=x2 or y1=y2
-# vent_inds = np.where((X[0]==X[1]) | (Y[0]==Y[1]))
-
-# x = np.array([X[0][vent_inds], X[1][vent_inds]], dtype='int')
-# y = np.array([Y[0][vent_inds], Y[1][vent_inds]], dtype='int')
-x = np.copy(X)
-y = np.copy(Y)
+x = np.array([C[:,0], C[:,2]], dtype='int')
+y = np.array([C[:,1], C[:,3]], dtype='int')
 
 # Map of vents
 vents = np.zeros([int(y.max())+1, int(x.max())+1])
@@ -42,6 +34,7 @@ for x1, x2, y1, y2 in zip(x[0], x[1], y[0], y[1]):
         vents[y1, x1_coord:x2_coord+1] += 1
     # Diagonal line
     else:
+        # Create array of coordinates
         if x1>x2: 
             x_array = np.arange(x1, x2-1, -1)
         elif x1<x2:
@@ -51,20 +44,13 @@ for x1, x2, y1, y2 in zip(x[0], x[1], y[0], y[1]):
             y_array = np.arange(y1, y2-1, -1)
         elif y1<y2:
             y_array = np.arange(y1, y2+1)
-        # Create array of coordinates
-        # y_array = np.arange(y1_coord, y2_coord+1)
-        # x_array = np.arange(x1_coord, x2_coord+1)
+        
         # Add 1 to each coordinate point
         vents[y_array, x_array] += 1
             
-        
 # Calculate number of points where lines overlap
 n_points = (vents>1).sum()
 print(f'Lines cross at {n_points} points.')
-
-'''
-Part 2
-'''
 
 
 
